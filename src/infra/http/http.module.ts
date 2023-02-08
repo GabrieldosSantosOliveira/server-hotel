3d66cf07fe6317412bf213f3a685861b65315026
Gabriel dos Santos Oliveira <gabrielsantosoliveira951@gmail.com>
2023-02-08 13:34:43 -0400

    feat: update module http

--- src/infra/http/http.module.ts
+++ src/infra/http/http.module.ts
@@ -1,14 +1,38 @@
+import { IMailProvider } from '@application/repositories/mail-repository';
+import { EmailUserSingUp } from '@application/use-cases/sign-up/email-user-sing-up';
+import { GoogleUserSingUp } from '@application/use-cases/sign-up/google-user-sing-up';
+import { AuthModule } from '@auth/auth.module';
+import { NodeMailerProvider } from '@infra/implementations/mail/nodemalier-provider';
 import { HttpModule as HttpModuleAxios } from '@nestjs/axios';
 import { Module } from '@nestjs/common';
-import { CreateUserUseCase } from 'src/application/use-cases/Create-user';
 
 import { DatabaseModule } from '../database/database.module';
-import { UserController } from './controllers/user.controller';
+import { EmailSingUpController } from './controllers/Auth/email-sing-up.controller';
+import { GoogleSingUpController } from './controllers/Auth/google-sing-up.controller';
 
 @Module({
-  imports: [DatabaseModule, HttpModuleAxios],
-  controllers: [UserController],
-  providers: [CreateUserUseCase],
+  imports: [AuthModule, DatabaseModule, HttpModuleAxios],
+  controllers: [EmailSingUpController, GoogleSingUpController],
+  providers: [
+    GoogleUserSingUp,
+    EmailUserSingUp,
+    {
+      provide: IMailProvider,
+      useFactory: () => {
+        return new NodeMailerProvider({
+          transport: {
+            host: 'sandbox.smtp.mailtrap.io',
+            port: 465,
+            secure: false,
+            auth: {
+              user: '237f6cddc7bd86',
+              pass: '5f2205551637fe',
+            },
+          },
+        });
+      },
+    },
+  ],
   exports: [],
 })
 export class HttpModule {}
