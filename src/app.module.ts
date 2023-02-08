08856e87ecfb75ddc8489f597bc27e3ec336d81f
Gabriel dos Santos Oliveira <gabrielsantosoliveira951@gmail.com>
2023-02-08 13:00:59 -0400

    feat: Add ConfigModule for load environment variables

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 
 import { DatabaseModule } from './infra/database/database.module';
 import { HttpModule } from './infra/http/http.module';
 @Module({
-  imports: [DatabaseModule, HttpModule],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    DatabaseModule,
+    HttpModule,
+  ],
   controllers: [],
   providers: [],
 })
