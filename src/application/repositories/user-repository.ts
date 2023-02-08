8749d5f631fb5e8aa76750dd2bb5cdc82ef297ce
Gabriel dos Santos Oliveira <gabrielsantosoliveira951@gmail.com>
2023-02-08 13:10:47 -0400

    feat: update methods of UserRepository

--- src/application/repositories/user-repository.ts
+++ src/application/repositories/user-repository.ts
@@ -1,6 +1,14 @@
-import { User } from '../entities/User';
+import { User } from '../entities/user';
+export interface UserByEmailAndGoogleId {
+  email: string;
+  googleId: string;
+}
 
 export abstract class UserRepository {
   abstract create(user: User): Promise<void>;
-  abstract findByEmail(email: string): Promise<User>;
+  abstract findByEmail(email: string): Promise<User | null>;
+  abstract findByEmailAndGoogleId(
+    data: UserByEmailAndGoogleId,
+  ): Promise<User | null>;
+  abstract save(data: User): Promise<void>;
 }
