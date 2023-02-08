076f96fc6138408e96a38e11fccf3409b13f8a7d
Gabriel dos Santos Oliveira <gabrielsantosoliveira951@gmail.com>
2023-02-08 13:13:59 -0400

    feat: add class for comparer password

--- src/application/services/hash-comparer.ts
+++ src/application/services/hash-comparer.ts
@@ -1,5 +1,5 @@
 import { compare } from 'bcrypt';
-export class HashComaprer {
+export class HashComparer {
   static async compare(password: string, hash: string): Promise<boolean> {
     return await compare(password, hash);
   }
