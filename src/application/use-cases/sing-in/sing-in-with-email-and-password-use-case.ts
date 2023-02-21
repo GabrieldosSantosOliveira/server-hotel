2aa076c697ddb053880cd1bf29888b44532c6554
Gabriel dos Santos Oliveira <gabrielsantosoliveira951@gmail.com>
2023-02-21 17:18:18 -0400

    feat: add sing in with email and password use case

---
+++ src/application/use-cases/sing-in/sing-in-with-email-and-password-use-case.ts
@@ -0,0 +1,30 @@
+import { UserRepository } from '@application/repositories/user-repository';
+import { HashComparer } from '@application/services/hash-comparer';
+import { AuthService } from '@auth/auth.service';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+interface Request {
+  email: string;
+  password: string;
+}
+@Injectable()
+export class SingInWithEmailAndPasswordUseCase {
+  constructor(
+    private readonly userRepository: UserRepository,
+    private readonly authService: AuthService,
+  ) {}
+  async execute({ email, password }: Request) {
+    const userExist = await this.userRepository.findByEmail(email);
+    if (!userExist) {
+      throw new UnauthorizedException('Invalid password or email');
+    }
+    const isValidPassword = await HashComparer.compare({
+      password,
+      passwordHash: userExist.password,
+    });
+    console.log(!isValidPassword);
+    if (!isValidPassword) {
+      throw new UnauthorizedException('Invalid password or email');
+    }
+    return this.authService.generateAccessTokenAndRefreshToken(userExist);
+  }
+}
