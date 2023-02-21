6f47f3a7ee8f1d15ed6924821ac6d2b9675d7633
Gabriel dos Santos Oliveira <gabrielsantosoliveira951@gmail.com>
2023-02-21 17:23:23 -0400

    feat: add guard and strategy for refresh token

---
+++ src/auth/strategies/jwt/refresh-token.strategy.ts
@@ -0,0 +1,25 @@
+import { Injectable } from '@nestjs/common';
+import { PassportStrategy } from '@nestjs/passport';
+import { ExtractJwt, Strategy } from 'passport-jwt';
+type JwtPayload = {
+  sub: string;
+  iat: number;
+  exp: number;
+};
+@Injectable()
+export class RefreshTokenStrategy extends PassportStrategy(
+  Strategy,
+  'jwt-refresh-token',
+) {
+  constructor() {
+    super({
+      jwtFromRequest: ExtractJwt.fromBodyField('refresh_token'),
+      ignoreExpiration: false,
+      secretOrKey: process.env.SECRET_KEY_REFRESH_TOKEN,
+    });
+  }
+
+  async validate(payload: JwtPayload) {
+    return payload;
+  }
+}
