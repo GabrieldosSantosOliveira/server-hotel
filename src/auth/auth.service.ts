2746da867969516f9a74318df00b87e47b08243a
Gabriel dos Santos Oliveira <gabrielsantosoliveira951@gmail.com>
2023-02-21 17:21:01 -0400

    feat: add methods generateAccessToken, generateRefreshToken and generateAccessTokenAndRefreshToken

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -4,8 +4,27 @@ import { JwtService } from '@nestjs/jwt';
 @Injectable()
 export class AuthService {
   constructor(private readonly jwtService: JwtService) {}
-  generateToken(user: User) {
-    const token = this.jwtService.sign({ sub: user.id });
+
+  generateAccessToken(user: User) {
+    const token = this.jwtService.sign(
+      { sub: user.id },
+      { secret: process.env.SECRET_KEY_ACCESS_TOKEN, expiresIn: 60 },
+    );
     return { access_token: token };
+  }
+  generateRefreshToken(user: User) {
+    const token = this.jwtService.sign(
+      { sub: user.id },
+      {
+        expiresIn: 30 * 24 * 60 * 60,
+        secret: process.env.SECRET_KEY_REFRESH_TOKEN,
+      },
+    );
+    return { refresh_token: token };
+  }
+  generateAccessTokenAndRefreshToken(user: User) {
+    const { access_token } = this.generateAccessToken(user);
+    const { refresh_token } = this.generateRefreshToken(user);
+    return { access_token, refresh_token };
   }
 }
