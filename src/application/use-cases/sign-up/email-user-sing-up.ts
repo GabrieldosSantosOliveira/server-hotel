c9e13dffaa92452c927de65854ef1f09b4bf5e61
Gabriel dos Santos Oliveira <gabrielsantosoliveira951@gmail.com>
2023-02-21 17:14:58 -0400

    feat: add email user sing up use case

--- src/application/use-cases/sign-up/email-user-sing-up.ts
+++ src/application/use-cases/sign-up/email-user-sing-up.ts
@@ -3,9 +3,9 @@ import { Password } from '@application/entities/Password';
 import { Providers } from '@application/entities/Providers';
 import { IMailProvider } from '@application/repositories/mail-repository';
 import { UserRepository } from '@application/repositories/user-repository';
+import { generateRandomNumber } from '@application/services/generate-random-number';
 import { ConflictException, Injectable } from '@nestjs/common';
 import * as dayjs from 'dayjs';
-import { randomUUID } from 'node:crypto';
 
 import { User } from '../../entities/User';
 type RequestEmailUserSingUp = {
@@ -21,7 +21,26 @@ export class EmailUserSingUp {
   async execute({ email, password }: RequestEmailUserSingUp): Promise<User> {
     const user = await this.userRepository.findByEmail(email);
     if (user) {
-      throw new ConflictException('User already exists');
+      if (!user.isCompleteRegister) {
+        user.validEmailExpires = dayjs().add(1, 'hour').toDate();
+        user.validEmailToken = generateRandomNumber({
+          max: 999999,
+          min: 100000,
+        }).toString();
+        await this.userRepository.save(user);
+        await this.mailProvider.sendEmail({
+          body: `<div><a href="https://validate-email-iota.vercel.app/g">Usuario</a> token: ${user.validEmailToken} </div>`,
+          subject: 'Usuário criado com sucesso',
+          to: email,
+          from: {
+            name: 'Equipe de suporte',
+            email: 'no_reply@reasa.com.br',
+          },
+        });
+        return user;
+      } else {
+        throw new ConflictException('User already exists');
+      }
     }
     const userDomain = new User({
       email: Email.create(email),
@@ -32,12 +51,15 @@ export class EmailUserSingUp {
       verifiedEmail: false,
       isCompleteRegister: false,
       validEmailExpires: dayjs().add(1, 'hour').toDate(),
-      validEmailToken: randomUUID(),
+      validEmailToken: generateRandomNumber({
+        max: 999999,
+        min: 100000,
+      }).toString(),
     });
 
     await this.userRepository.create(userDomain);
     await this.mailProvider.sendEmail({
-      body: '<a href="https://google.com.br">Usuario</a>',
+      body: `<div><a href="https://validate-email-iota.vercel.app/g">Usuario</a> token: ${userDomain.validEmailToken} </div>`,
       subject: 'Usuário criado com sucesso',
       to: email,
       from: {
