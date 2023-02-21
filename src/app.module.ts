3f547c26b28ae358b634a53b126af23aa9aafba9
Gabriel dos Santos Oliveira <gabrielsantosoliveira951@gmail.com>
2023-02-21 16:55:31 -0400

    feat: import LoggerModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { LoggerModule } from 'nestjs-pino';
 
 import { DatabaseModule } from './infra/database/database.module';
 import { HttpModule } from './infra/http/http.module';
 @Module({
   imports: [
+    LoggerModule.forRoot(),
     ConfigModule.forRoot({ isGlobal: true }),
     DatabaseModule,
     HttpModule,
