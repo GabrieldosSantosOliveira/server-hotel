43f6c0d957a8f52949f75def9238f8ae729af9e7
Gabriel dos Santos Oliveira <gabrielsantosoliveira951@gmail.com>
2023-02-21 17:25:22 -0400

    feat: getting key from environment variables

--- src/auth/strategies/jwt/jwt.strategy.ts
+++ src/auth/strategies/jwt/jwt.strategy.ts
@@ -1,16 +1,22 @@
 import { Injectable } from '@nestjs/common';
 import { PassportStrategy } from '@nestjs/passport';
 import { ExtractJwt, Strategy } from 'passport-jwt';
+type JwtPayload = {
+  sub: string;
+  iat: number;
+  exp: number;
+};
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy) {
   constructor() {
     super({
       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
       ignoreExpiration: false,
-      secretOrKey: 'jjjj',
+      secretOrKey: process.env.SECRET_KEY_ACCESS_TOKEN,
     });
   }
-  async validate(payload: any) {
-    return { userId: payload.sub, username: payload.username };
+
+  async validate(payload: JwtPayload) {
+    return payload;
   }
 }
