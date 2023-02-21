b644c8bbef0d8c60c9f1559f55faba6b738299b3
Gabriel dos Santos Oliveira <gabrielsantosoliveira951@gmail.com>
2023-02-21 17:00:07 -0400

    feat: add method findById in UserRepository

--- src/application/repositories/user-repository.ts
+++ src/application/repositories/user-repository.ts
@@ -7,6 +7,7 @@ export interface UserByEmailAndGoogleId {
 export abstract class UserRepository {
   abstract create(user: User): Promise<void>;
   abstract findByEmail(email: string): Promise<User | null>;
+  abstract findById(id: string): Promise<User | null>;
   abstract findByEmailAndGoogleId(
     data: UserByEmailAndGoogleId,
   ): Promise<User | null>;
