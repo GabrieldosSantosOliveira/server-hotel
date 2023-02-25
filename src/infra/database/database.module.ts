d93c2b0cee4f0fd5e819abc3d534e381d514b0e7
Gabriel dos Santos Oliveira <gabrielsantosoliveira951@gmail.com>
2023-02-25 16:21:56 -0400

    feat: add repository ImageRepository and FacilitieRepository in module database

--- src/infra/database/database.module.ts
+++ src/infra/database/database.module.ts
@@ -1,6 +1,10 @@
+import { FacilitieRepository } from '@application/repositories/facilitie-repository';
+import { ImageRepository } from '@application/repositories/image-repository';
 import { UserRepository } from '@application/repositories/user-repository';
 import { Module } from '@nestjs/common';
 
+import { PrismaFacilitieRepository } from './prisma/prisma-facilitie-repository';
+import { PrismaImageRepository } from './prisma/prisma-image-repository';
 import { PrismaUserRepository } from './prisma/prisma-user-reporitory';
 import { PrismaService } from './prisma/service.service';
 
@@ -11,7 +15,15 @@ import { PrismaService } from './prisma/service.service';
       provide: UserRepository,
       useClass: PrismaUserRepository,
     },
+    {
+      provide: ImageRepository,
+      useClass: PrismaImageRepository,
+    },
+    {
+      provide: FacilitieRepository,
+      useClass: PrismaFacilitieRepository,
+    },
   ],
-  exports: [UserRepository],
+  exports: [UserRepository, ImageRepository, FacilitieRepository],
 })
 export class DatabaseModule {}
